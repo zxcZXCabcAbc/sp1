f58f45c484a69e0bb0fed6cf6c312735f352df62
lifeixiang <605951579@qq.com>
2024-04-23 14:50:01 +0800

    队列实现paypal支付

--- app/controller/NotifyController.php
+++ app/controller/NotifyController.php
@@ -31,15 +31,14 @@ class NotifyController extends BaseController
         tplog('checkout',$params);
         $notifyData =  ['order_id'=>$order->id,'params'=>$params,'created_at'=>Carbon::now()->toDateTimeString(),'type'=>Notify::TYPE_CHECKOUT];
         $order->notifies()->save($notifyData);
-        echo 'success';
         //队列处理
-//        Queue::push(CapturePaymentQueue::class,['order_id'=>$order->id,'request'=>$request->all()],'checkout');
-//        $host = $order->shop->host;
-//        $path = 'checkout';
-//        $query = ['order_id'=>$order->id];
-//        $query = array_merge($request->all(),$query);
-//        $url = sprintf('%s/%s?%s',$host,$path,http_build_query($query));
-//        return redirect(domain($url));
+        Queue::push(CapturePaymentQueue::class,['order_id'=>$order->id,'request'=>$request->all()],'checkout');
+        $host = $order->shop->host;
+        $path = 'checkout';
+        $query = ['order_id'=>$order->id];
+        $query = array_merge($request->all(),$query);
+        $url = sprintf('%s/%s?%s',$host,$path,http_build_query($query));
+        return redirect(domain($url));
     }
 
 
