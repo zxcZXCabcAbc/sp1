d0d91487e8fca2317144470bc6429bcb43587238
lifeixiang <605951579@qq.com>
2024-05-15 17:29:43 +0800

    优化日志增加order_id,draft_id

--- app/model/OrderLogs.php
+++ app/model/OrderLogs.php
@@ -7,12 +7,17 @@ use think\Model;
 use think\Request;
 
 /**
- * @mixin \think\Model
+ * @property string $checkout_id
+ * @property string $logs
+ * @property string $created_at
+ * @property string $draft_id
+ * @property integer $order_id
+ * @property integer $shop_id
  */
 class OrderLogs extends Model
 {
     protected $field = [
-        'checkout_id','logs','created_at','shop_id'
+        'checkout_id','logs','created_at','shop_id','order_id','draft_id'
     ];
 
     public $autoWriteTimestamp = false;
@@ -23,6 +28,8 @@ class OrderLogs extends Model
             'checkout_id'=>$request->param('checkout_id'),
             'logs'=>$request->param('logs'),
             'shop_id'=>$request->middleware('x_shop_id'),
+            'order_id'=>$request->middleware('order_id',0),
+            'draft_id'=>$request->middleware('draft_id',''),
             'created_at'=>date('Y-m-d H:i:s'),
         ]);
     }
